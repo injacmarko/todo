8462977ed46cb32455905dba284175c7f67eb8ef
Praktikant 9 <praktikant_9@vega.local>
2022-09-19 16:41:55 +0200

    Dodat atribut isComplete u TodoItem

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -4,6 +4,7 @@ namespace TodoApi.Models
     {
         public long Id { get; set; }
         public string? Title { get; set; }
+        public bool isComplete{ get; set; }
         public string? Date { get; set; }
     }
 }
