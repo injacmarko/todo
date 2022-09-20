713944ff7948fed5f58eed45bd4959fa54b9d55f
Praktikant 9 <praktikant_9@vega.local>
2022-09-20 10:38:20 +0200

    baza

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -3,14 +3,10 @@ using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext")));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
